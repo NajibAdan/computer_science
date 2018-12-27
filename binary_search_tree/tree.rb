af961d2fa0d087ba73c962b7f29befccf7834563
Najib Adan <lewdwarrior@waifu.club>
2018-12-27 14:37:30 +0300

    Added comments

--- binary_search_tree/tree.rb
+++ binary_search_tree/tree.rb
@@ -2,6 +2,8 @@ require './node.rb'
 
 class Tree
     attr_accessor :root
+
+    #builds a tree given an array
     def build_tree(array)
         @root = Node.new(array.shift)
         array.each do |i|
@@ -9,6 +11,7 @@ class Tree
         end
     end
 
+    #creates a node and inserts to the tree depending on its value
     def insert(node,val)
         if (node.data > val)
             node.left ? insert(node.left,val) : node.left = Node.new(val)
@@ -17,6 +20,7 @@ class Tree
         end
     end
 
+    #performs the breadth first scan
     def breadth_first_search(val)
         tracker = [@root]
         until tracker.empty?
@@ -28,6 +32,7 @@ class Tree
         return nil
     end
 
+    #perfroms depth first scan
     def depth_first_search(val)
         tracker = [@root]
         until tracker.empty?
@@ -39,6 +44,7 @@ class Tree
         return nil
     end
 
+    #a recursive implementation of the depth first scan
     def dfs_recs(data,node=@root)
         return "#{data} found at #{node}" if node.data == data
         dfs_recs(data,node.left) if node.left
